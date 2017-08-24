5ab7143419b76a2ebb53f089275c819733683245
isaias pereira <isaias.pereira0003@hotmail.com>
2017-08-23 22:24:00 -0300

    Adicionar arquivos de projeto.

---
+++ WebAppAula2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppAula2.Startup))]
+namespace WebAppAula2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
